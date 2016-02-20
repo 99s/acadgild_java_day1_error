6580384d30e89776c5705c8c38e18c3fc600a76d
99s <divi9e@gmail.com>
2016-02-20 16:42:07 +0530

    Added files via upload
    import java.io.DataInputStream;
    class error
     {
    	public static void main(String args[])
    	{
    	 
    	 /*the following cannot be done
    	 out of range , 
    	 exact error prompt = possible loss of precision
    	 */
    	 short s;
    	 s = 99999999;
    	 //also
    	 int i ;
    	 i = true;
    	 i = rio;
    	 i = 2147483648;
    	 System.out.println(" s value : " +s );
    	}
     }

---
+++ error.java
@@ -0,0 +1,20 @@
+import java.io.DataInputStream;
+class error
+ {
+	public static void main(String args[])
+	{
+	 
+	 /*the following cannot be done
+	 out of range , 
+	 exact error prompt = possible loss of precision
+	 */
+	 short s;
+	 s = 99999999;
+	 //also
+	 int i ;
+	 i = true;
+	 i = rio;
+	 i = 2147483648;
+	 System.out.println(" s value : " +s );
+	}
+ }
